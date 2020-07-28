3d7d2ca4de199590423123239a05effe577318a5
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-07-29 09:42:26 +1000

    add day-17*.js

---
+++ day-17_rotate_matrix.js
@@ -0,0 +1,57 @@
+// Rotating Matrix
+
+// let matrix = [
+//   [1, 5, 8],
+//   [4, 7, 2],
+//   [3, 9, 6],
+// ];
+
+// The above matrix rotated 90 degree clockwise:
+// 3 4 1
+// 9 7 5
+// 6 2 8
+
+// a non-square matrix rotated:
+// matrix:
+// 3 4 1
+// 9 7 5
+// Rotated 90 degree clockwise:
+// 9 3
+// 7 4
+// 5 1
+
+// Write a function that takes an arbitrary MxN matrix, rotates it clockwise
+// by 90-degrees as described above, and returns the result as a new matrix.
+// The function should not mutate the original matrix.
+
+function rotate90(matrix) {
+  const rotatedMatrix = [];
+  const rowCount = matrix[0].length;
+  for (let rowIndex = 0; rowIndex < rowCount; rowIndex += 1) {
+    rotatedMatrix.push([]);
+    for (let colIndex = matrix.length - 1; colIndex >= 0; colIndex -= 1) {
+      rotatedMatrix[rowIndex].push(matrix[colIndex][rowIndex]);
+    }
+  }
+
+  return rotatedMatrix;
+}
+
+let matrix1 = [
+  [1, 5, 8],
+  [4, 7, 2],
+  [3, 9, 6],
+];
+
+let matrix2 = [
+  [3, 7, 4, 2],
+  [5, 1, 0, 8],
+];
+
+let newMatrix1 = rotate90(matrix1);
+let newMatrix2 = rotate90(matrix2);
+let newMatrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))));
+
+console.log(newMatrix1); // [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+console.log(newMatrix2); // [[5, 3], [1, 7], [0, 4], [8, 2]]
+console.log(newMatrix3); // `matrix2` --> [[3, 7, 4, 2], [5, 1, 0, 8]]
