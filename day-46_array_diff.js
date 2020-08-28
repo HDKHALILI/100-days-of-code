6c07520f725d3473ea1de851dd25dd430856920e
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-08-28 22:05:11 +1000

    add another solution to day-46*.js

--- day-46_array_diff.js
+++ day-46_array_diff.js
@@ -24,8 +24,13 @@ function arrayDiff(a, b) {
   return aCopy;
 }
 
-console.log(array_diff([1, 2], [1])); // [2], "a was [1,2], b was [1]")
-console.log(array_diff([1, 2, 2], [1])); // [2,2], "a was [1,2,2], b was [1]")
-console.log(array_diff([1, 2, 2], [2])); // [1], "a was [1,2,2], b was [2]")
-console.log(array_diff([1, 2, 2], [])); // [1,2,2], "a was [1,2,2], b was []")
-console.log(array_diff([], [1, 2])); // [], "a was [], b was [1,2]")
+// using filter
+function arrayDiff2(a, b) {
+  return a.filter((ele) => !b.includes(ele));
+}
+
+console.log(arrayDiff([1, 2], [1])); // [2], "a was [1,2], b was [1]")
+console.log(arrayDiff([1, 2, 2], [1])); // [2,2], "a was [1,2,2], b was [1]")
+console.log(arrayDiff([1, 2, 2], [2])); // [1], "a was [1,2,2], b was [2]")
+console.log(arrayDiff([1, 2, 2], [])); // [1,2,2], "a was [1,2,2], b was []")
+console.log(arrayDiff([], [1, 2])); // [], "a was [], b was [1,2]")
