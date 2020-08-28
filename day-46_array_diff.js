5b807eb431979447c73c6cdfea05710d4b767a5f
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-08-28 22:01:49 +1000

    add test cases to day-46*.js

--- day-46_array_diff.js
+++ day-46_array_diff.js
@@ -23,3 +23,9 @@ function arrayDiff(a, b) {
 
   return aCopy;
 }
+
+console.log(array_diff([1, 2], [1])); // [2], "a was [1,2], b was [1]")
+console.log(array_diff([1, 2, 2], [1])); // [2,2], "a was [1,2,2], b was [1]")
+console.log(array_diff([1, 2, 2], [2])); // [1], "a was [1,2,2], b was [2]")
+console.log(array_diff([1, 2, 2], [])); // [1,2,2], "a was [1,2,2], b was []")
+console.log(array_diff([], [1, 2])); // [], "a was [], b was [1,2]")
