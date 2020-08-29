1c6d4f9acf5fe035be5924110b694d3f76c0557f
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-08-29 20:00:13 +1000

    add test cases to day-47*.js

--- day-47_sum_of_lowest_positive_integer.js
+++ day-47_sum_of_lowest_positive_integer.js
@@ -11,3 +11,9 @@ function sumTwoSmallestNumbers(numbers) {
   let twoLowestIntegers = numbers.sor((a, b) => a - b).slice(0, 2);
   return twoLowestIntegers.reduce((sum, num) => sum + num, 0);
 }
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22])); // 13 , "Sum should be 13"
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43])); // 6 , "Sum should be 6"
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7])); // 10 , "Sum should be 10"
+console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1])); // 24 , "Sum should be 24"
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4])); // 16 , "Sum should be 16"
